20592b71f9660d12d8c8286561cd41cea5de401f
root <root@dm1400754584699.(none)>
2014-07-06 22:03:50 +0900

    log config added, map data added

---
+++ Data.py
@@ -0,0 +1,10 @@
+# encoding:utf-8
+import random
+
+MAX_GROWTH = 1
+MAP_SIZE = 100
+
+GrowthMap = [[random.randrange(MAX_GROWTH) for i in xrange(MAP_SIZE)] for j in xrange(MAP_SIZE)]
+
+if __name__ == '__main__':
+    print GrowthMap
